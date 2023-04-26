5ed55f963a8f4c1e6c82c1680b263a4058aa4135
cta <125872009+themightycta@users.noreply.github.com>
2023-04-26 09:54:43 +0200

    Aggiornato database con più razze
    Una razza un più

--- setup.py
+++ setup.py
@@ -1,7 +1,38 @@
-razze = "Dalmata", "Golden Retriever", "Labrador Retriever", "Bassotto", "Alano", "Pastore Tedesco", "Akita", "Barbone nano", "Barbone standard", "Barbone Toy", "Bobtail", "Border Collie", "Border Terrier", "Borzoi", "Boston Terrier", "Bovaro del Bernese", "Bovaro delle Fiandre", "Boxer", "Bracco Italiano", "Bull Terrier", "Bulldog", "Bulldog Francese", "Cairn Terrier", "Canaan Dog", "Cane da Montagna dei Pirenei", "Cane dei faraoni", "Carlino", "Chihuahua pelo corto", "Chihuahua pelo lungo"
+razze_separate = [
+    "Dalmata",
+    "Golden Retriever",
+    "Labrador Retriever",
+    "Bassotto",
+    "Alano",
+    "Pastore Tedesco",
+    "Akita",
+    "Barbone nano",
+    "Barbone standard",
+    "Barbone Toy",
+    "Bobtail",
+    "Border Collie",
+    "Border Terrier",
+    "Borzoi",
+    "Boston Terrier",
+    "Bovaro del Bernese",
+    "Bovaro delle Fiandre",
+    "Boxer",
+    "Bracco Italiano",
+    "Bull Terrier",
+    "Bulldog",
+    "Bulldog Francese",
+    "Cairn Terrier",
+    "Canaan Dog",
+    "Cane da Montagna dei Pirenei",
+    "Cane dei faraoni",
+    "Carlino",
+    "Chihuahua pelo corto",
+    "Chihuahua pelo lungo",
+    "Yorkshire Terrier"
+]
 
 database = {
-'Razze cani:' : razze
+'Razze cani:' : razze_separate
 }
 
 def razza(razzadata):
